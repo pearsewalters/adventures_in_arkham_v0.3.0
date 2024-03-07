1880361852fa26f356627a6c6ae52b8f9a436ce1
Samuel Pearse Walters <samuel.p.walters@gmail.com>
2024-03-07 14:00:11 -0800

    messing around with interaction and game procedure

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ from icecream import ic
 
 # game defaults
 
-INVESTIGATOR = "Amanda Sharpe"
+INVESTIGATOR = "Sister Mary"
 ANCIENT_ONE = "Azathoth"
 
 def azathoth_rules():
@@ -159,22 +159,74 @@ def arkham_location_occupants_of_dimension( locs, dim ):
         for occupant in location_occupants_of_dimension( loc, dim ):
             yield occupant, loc
 
-def normal_monster_move( adj_matrix, dim, loc ):
-    if loc != 38: # ignore the outskirts
-        # find adjacency
-        adj = adj_matrix[loc].index( 1 )
-        # remove current location from index
-        board.board_transforms['monster_locations'].append( (board.remove_monster_location, dim, loc ) )
-        # add future location to index
-        board.board_transforms['monster_locations'].append( (board.add_monster_location, dim, adj ) )
-        return adj
+def normal_monster_move( monster, adj_matrix, dim, loc ):
+    # find adjacency
+    adj = adj_matrix[loc].index( 1 )
+    # remove current location from index
+    board.board_transforms['monster_locations'].append( (board.remove_monster_location, dim, loc ) )
+    # add future location to index
+    board.board_transforms['monster_locations'].append( (board.add_monster_location, dim, adj ) )
+    print( f'{monster} has moved from {locations.all_neighbors[loc][0]} to {locations.all_neighbors[adj][0]}.')
     
-def move_monsters():
-    groups = [ (0,), (4,), (5,), (8,), (2,3), (1,6,7) ]
-    distros = [ 11, 11, 11, 11, 11, 11 ]
+def fast_monster_move( monster, adj_matrix, dim, loc ):
+    """This is normal monster movement with the square of the adjacency matrix"""
 
+    squared_adj = tools.matrix_square( [ row[1:] for row in adj_matrix[1:] ] )
+    # put the labels back in
+    for a,b in zip( squared_adj, adj_matrix[1:] ):
+        a.insert( 0, b[0] )
+    squared_adj.insert( 0, adj_matrix[0] )
+
+    normal_monster_move( monster, squared_adj, dim, loc )
+
+def stationary_monster_move( *args ):
+    """This doesn't move the monster, but receives args for compatibility."""
+    return None
+
+def flying_monster_move( monster, adj_matrix, dim, loc ):
+    """This will one day have implementation..."""
+    ...
+    return NotImplemented
+
+def chthonian_monster_move( monster, adj_matrix, dim, loc ):
+    """This will one day have implementation..."""
+    ...
+    return NotImplemented
+
+def hound_monster_move( monster, adj_matrix, dim, loc ):
+    """This will one day have implementation..."""
+    ...
+    return NotImplemented
+
+def move_monsters():
+    # monsters move in groups based on their dimension
+    groups = [ (0,), (4,), (5,), (8,), (2,3), (1,6,7) ]
+    # each group has likelihood of appearing, i do this so i can use rand_from_distro 
+    distros = [ 11, 11, 11, 11, 11, 11 ]
+    # randomly select a group to move left...
     move_left = tools.rand_from_distro( dict(zip( groups, distros )) )
+    # ...and of the remaining groups randomly select one to go right
     move_right = tools.rand_from_distro( { k:v for k,v in dict(zip( groups, distros )).items() if k != move_left } )
+
+    rules = {
+        0 : normal_monster_move,
+        1 : fast_monster_move,
+        2 : stationary_monster_move,
+        3 : flying_monster_move,
+        4 : chthonian_monster_move,
+        5 : hound_monster_move
+    }
+
+    def move_by_rule( rule, *args ):
+        rules = {
+            0 : normal_monster_move,
+            1 : fast_monster_move,
+            2 : stationary_monster_move,
+            3 : flying_monster_move,
+            4 : chthonian_monster_move,
+            5 : hound_monster_move
+        }
+        return rules[rule]( *args )
 
     for dim, locs in enumerate( board.current_monst_locs_by_dim( board.board_defaults['monster_locations'], board.board_transforms['monster_locations' ] ) ): 
         if dim in move_left and len( locs ):
@@ -182,19 +234,15 @@ def move_monsters():
                 # check monster movement rule
                 monster_in_situ = monster_defs_trans( occupant, zip( monsters.monster_defaults, monsters.monster_transforms ) )
                 mvmt_rule = monsters.current_rules( monster_in_situ['defaults'][1], monster_in_situ['transforms'][1] )[0]
-                if mvmt_rule == 0:
-                    adj = normal_monster_move( locations.left_graph, dim, loc_id )
-                    print( f'{monster_in_situ["defaults"][0]} has moved from {locations.neighbors_graph[loc_id][0]} to {locations.neighbors_graph[adj][0]}.')
+                rules[mvmt_rule](occupant, locations.left_neighbors, dim, loc_id )   
         elif dim in move_right and len( locs ):
             for occupant, loc_id in arkham_location_occupants_of_dimension( locs, dim ):
                 # check monster movement rule
                 monster_in_situ = monster_defs_trans( occupant, zip( monsters.monster_defaults, monsters.monster_transforms ) )
                 mvmt_rule = monsters.current_rules( monster_in_situ['defaults'][1], monster_in_situ['transforms'][1] )[0]
-                if mvmt_rule == 0:
-                    adj = normal_monster_move( locations.right_graph, dim, loc_id )
-                    print( f'{monster_in_situ["defaults"][0]} has moved from {locations.neighbors_graph[loc_id][0]} to {locations.neighbors_graph[adj][0]}.')
+                rules[mvmt_rule](occupant, locations.right_neighbors, dim, loc_id )
 
-def mythos_gate():
+def mythos():
 
     gate_distro = {}
     clue_distro = {}
@@ -243,21 +291,176 @@ def mythos_gate():
             awaken_the_ancient_one()
     elif draw_gate == 2:
         print( 'A monster surge!' )
+        
+def investigator_defs_trans( inv ):
+    return location_defs_trans( inv, zip( investigator.investigator_defaults, investigator.investigator_transforms ) )
 
+def inv_constants( inv ):
+    return [ row for row in investigator.investigator_constants if row[0] == inv ][0]
+
+def show_skills():
+    """ Displays investigator skills"""
+
+    inv_constants( INVESTIGATOR )
+
+    nickname = 'TEST' # inv_constants( INVESTIGATOR )[1]
+    investigator_desc = investigator_defs_trans( INVESTIGATOR )
     
+    focus = investigator.current_skill( investigator_desc['defaults'][4], investigator_desc['transforms'][4] )[1]
+    mvmt = investigator.current_skill( investigator_desc['defaults'][4], investigator_desc['transforms'][4] )[2]
+    
+    speed = investigator.current_skill( investigator_desc['defaults'][5], investigator_desc['transforms'][5] )[1]
+    sneak = investigator.current_complement_skill( investigator_desc['defaults'][5], investigator_desc['transforms'][5] )
+
+    fight = investigator.current_skill( investigator_desc['defaults'][6], investigator_desc['transforms'][6] )[1]
+    will = investigator.current_complement_skill( investigator_desc['defaults'][6], investigator_desc['transforms'][6] )
+
+    lore = investigator.current_skill( investigator_desc['defaults'][7], investigator_desc['transforms'][7] )[1]
+    luck = investigator.current_complement_skill( investigator_desc['defaults'][7], investigator_desc['transforms'][7] )
+
+    display = f'{nickname}\'s Current Skills\n{'~'*20}\nFOCUS: {focus} {'.'*5} MVMT:  {mvmt}\nSPEED: {speed} {'.'*5} SNEAK: {sneak}\nFIGHT: {fight} {'.'*5} WILL:  {will}\nLORE:  {lore} {'.'*5} LUCK:  {luck}\n'
+
+    return display
+    
+def show_status():
+    """ Displays investigator health, location, and conditions """
+    nickname = inv_constants( INVESTIGATOR )[1]
+    investigator_desc = investigator_defs_trans( INVESTIGATOR )
+
+    display = f'{nickname}\'s Current Status\n{'~'*20}\n'
+
+    # health status
+    max_damage = investigator.current_stat( investigator_desc['defaults'][1], investigator_desc['transforms'][1] )[0]
+    current_damage = investigator.current_stat( investigator_desc['defaults'][1], investigator_desc['transforms'][1] )[1]
+    consciousness = 'unconscious' if investigator.current_stat( investigator_desc['defaults'][1], investigator_desc['transforms'][1] )[2] else 'conscious'
+    display_damage = f'{current_damage}/{max_damage}'
+
+    max_horror = investigator.current_stat( investigator_desc['defaults'][2], investigator_desc['transforms'][2] )[0]
+    current_horror = investigator.current_stat( investigator_desc['defaults'][2], investigator_desc['transforms'][2] )[1]
+    sanity = 'insane' if investigator.current_stat( investigator_desc['defaults'][2], investigator_desc['transforms'][2] )[2] else 'sane'
+    display_horror = f'{current_horror}/{max_horror}'
+
+    display += f'DAMAGE: {display_damage} {'.'*5} HORROR: {display_horror}\n'
+    display += f'{nickname} is currently {consciousness} and {sanity}\n\n'
+
+    # location 
+    location_id = investigator.current_location( investigator_desc['defaults'][8], investigator_desc['transforms'][8] )[0]
+    location_name = locations.location_constants[ location_id ][0]
+    location_neighborhood = locations.location_constants[ location_id][1]
+
+    display += f'LOCATION: {location_name}, in the {location_neighborhood} neighborhood\n\n'
+
+    # conditions 
+    conditions = investigator.current_condtions( investigator_desc['defaults'][3], investigator_desc['transforms'][3] )
+    display_conditions = ''
+
+    if conditions[1]:
+        if conditions[0]:
+            display_conditions += f'{nickname} is currently arrested\n'
+        elif conditions[2]:
+            display_conditions += f'{nickname} is currently lost in time & space\n'
+        else:
+            display_conditions += f'{nickname} is currently delayed\n'
+    if conditions[3]:
+        display_conditions += f'{nickname} is earning money from a retainer\n'
+    if conditions[4]:
+        display_conditions += f'{nickname} currently has a loan from the Bank of Arkham\n'
+    if conditions[5]:
+        display_conditions += f'{nickname} is a member of the Silver Twilight Lodge\n'
+    if conditions[6]:
+        display_conditions += f'{nickname} is the Deputy of Arkham. Monsters beware!\n'
+    if not conditions[7] + 1:
+        display_conditions += f'{nickname} is cursed!'
+    if not conditions[7] - 1:
+        display_conditions += f'{nickname} is blessed!'
+
+    display += display_conditions
+
+    return display
+
+def show_possessions():
+    """ Displays investigator possessions """
+
+    nickname = inv_constants( INVESTIGATOR )[1]
+    investigator_desc = investigator_defs_trans( INVESTIGATOR )
+
+    possessions = investigator.current_possessions( investigator_desc['defaults'][12], investigator_desc['transforms'][12] )
+
+    display = f'{nickname}\'s Current Possessions\n{'~'*20}\n'
+
+    display += f'MONEY: {possessions['money']} {'.'*5} CLUES: {possessions['clues']}\n'
+
+    def add_poss_to_disp( poss, header ):
+        d = ''
+        if len( poss ):
+            d += f'{header}: \n '
+            for item in poss:
+                d += f'{' '*len(header)} - {item}\n'
+            return d + '\n'
+        return d
+    
+    display += add_poss_to_disp( possessions['common'],'COMMON ITEMS')
+    display += add_poss_to_disp( possessions['unique'],'UNIQUE ITEMS')
+    display += add_poss_to_disp( possessions['spells'],'SPELLS')
+    display += add_poss_to_disp( possessions['buffs'],'LEARNED SKILLS')
+    display += add_poss_to_disp( possessions['allies'],'ALLIES')
+
+    return display + '\n'
+
+def quit():
+    ask = input( 'Are you sure you want to quit? (y/n) ' ).strip().lower()
+    if ask == 'y':
+        return True
+    return False
+
+def begin( phase ):
+    phases = { 
+        'upkeep' : None, 
+        'movement' : None,
+        'encounters' : None,
+        'mythos' : mythos 
+    }
+    if phase not in phases:
+        print( 'That is not a valid phase. Try again.' )
+        return False
+    return phases[phase]()
+    
+def show( view ):
+    """ Dispatch for show_view functions """
+    views = {
+        'skills' : show_skills,
+        'status' : show_status,
+        'possessions' : show_possessions
+    }
+    if view not in views:
+        return 'That is not a valid view. Try again.' 
+    return views[view]()
 
 
-# simulate 10 mythos phases
-for m in range(10):
-    mythos_gate()
-    print()
+valid_commands = {
+    'quit' : quit,
+    'begin': begin,
+    'show' : show
+}
 
-# for loc in zip( locations.location_defaults[1:], locations.location_transforms[1:] ):
-#     name = loc[0][0]
-#     occupants = locations.current_location_occupants( loc[0][1], loc[1][1] )
-#     status_values = locations.current_location_status( loc[0][2], loc[1][2] )
-#     status_labels = [ 'clues', 'historical clues', 'sealed', 'gate', 'explored', 'closed' ]
-#     status = dict( zip( status_labels, status_values ) )
-#     ic( name, occupants, status )
- 
-ic( board.current_monst_locs_by_dim( board.board_defaults['monster_locations'], board.board_transforms['monster_locations' ] ) )
+while True:
+    sentence = input( f'\x1b[38;5;70m>>> ' ).strip().lower().split(' ')
+    print('\n')
+    command = sentence[0]
+    arguments = sentence[1:]
+    if command not in valid_commands:
+        print( 'Hmm...unsure what you want. Try again. Type "help" to see a list of valid commands.' )
+    elif command == 'quit':
+        if quit():
+            break
+    else:
+        try:
+            if len( arguments ):
+                msg = valid_commands[command]( *arguments )
+                # if command return a message, print it 
+                if msg:
+                    print( msg )
+            else:
+                valid_commands[command]()
+        except TypeError as error:
+            print( error )
