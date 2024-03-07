efd77c1f4b4d6ae5516ec647c111c2eb9cb1f0f5
Samuel Pearse Walters <samuel.p.walters@gmail.com>
2024-03-07 13:57:28 -0800

    added matrix math for map

--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 # odds and ends that help 
 
 import random 
+from icecream import ic
 
 def rand_from_distro( distro: dict ) -> str:
     """Returns a random key using the values to determine likelihood"""
@@ -10,3 +11,17 @@ def rand_from_distro( distro: dict ) -> str:
         v += distro[k]/total
         if v >= r:
             return k
+        
+def dot_product( a, b ):
+    """The dot product of two vectors"""
+    return sum( [ a_n * b_n for a_n, b_n in zip( a, b) ] )
+
+def matrix_multiply( A, B ):
+    result = [ [] for row in A ]
+    for i, r in enumerate( A ):
+        for j, element in enumerate( r ):
+            result[i].append( dot_product( r, [ s[j] for s in B ] ) )
+    return result
+
+def matrix_square( A ):
+    return matrix_multiply( A, A )
