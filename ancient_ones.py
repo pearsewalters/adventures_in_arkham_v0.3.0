1092fdce72e583a3b26ccc2f2659efadd04d1a33
Samuel Pearse Walters <samuel.p.walters@gmail.com>
2024-03-25 13:43:05 -0700

    added in azathoth's rule changes

--- ancient_ones.py
+++ ancient_ones.py
@@ -1,6 +1,15 @@
-# constants
+from context import Context
+import tools, transformers
 
-ancient_one_constants = [
-    ['name', 'title'],
-    ['Azathoth', 'the Daemon Sultan'],
-]
+def azathoth_rules( context: Context ):
+    """Apply ancient one rules for Azathoth"""
+   
+    # maniacs have their toughness increased by 1
+    context.monsters.transforms.row('MANIAC').stats += [ transformers.inc_toughness]
+    
+    # doom track is set to 14
+    tools.set_limit( context.board.transforms['doom_track'], transformers.dec_doom_track, 14 )
+    
+RULES = {
+    'AZATHOTH' : azathoth_rules
+}
